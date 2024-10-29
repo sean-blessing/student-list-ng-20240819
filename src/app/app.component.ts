041526c081e227eafab40320fb7c05093e5fa307
Sean Blessing <snblessing@gmail.com>
2024-10-29 16:44:54 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['h1 {color: red;}'],
 })
 export class AppComponent {
   title = 'student-list';
---
+++ src/app/student-list/student-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent {
+  title: string = 'Student List';
+
+}
